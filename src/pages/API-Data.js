1ea1e4e8177eefaf7946355af7c82a2e8ba2a3c7
mahmoud-khader <mahmoudkhader2010@gmail.com>
2021-08-01 00:59:01 +0300

    test

--- src/pages/API-Data.js
+++ src/pages/API-Data.js
@@ -1,18 +1,55 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Jumbotron from 'react-bootstrap/Jumbotron';
+import axios from 'axios';
+import { withAuth0 } from '@auth0/auth0-react';
+
 
 class MyFavoriteBooks extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      url:process.env.REACT_APP_SERVER,
+      dataList:[],
+      showData:false,
+    }
+
+  }
+
+componentDidMount=async()=>{
+  axios.get(`${this.state.url}/api`).then(result=>{
+    this.setState({
+      dataList:result.data,
+      showData:true,
+    })
+  })
+}
+
+creatFav = async(item)=>{
+const reqbody={
+  email:this.props.auth0.user.email,
+  name:item.name,
+  img:item.img,
+  level:item.level
+};
+axios.post(`${this.state.url}/fav`,reqbody)
+}
+
   render() {
     return(
-      <Jumbotron>
-        <h1>All Data from the API</h1>
-        <p>
-          Select tour favorites :)
-        </p>
-      </Jumbotron>
+      <>
+{this.state.showData && this.state.dataList.map((item,index)=>{
+  return(
+    <>
+    <h2>{item.name}</h2>
+    <h2>{item.level}</h2>
+    <img src={item.img}/>
+    <button onClick={()=>{this.creatFav(item)}} >add to fav</button>
+    </>
+  )
+})}
+      </>
     )
   }
 }
 
-export default MyFavoriteBooks;
+export default withAuth0(MyFavoriteBooks);
--- src/pages/MyFav.js
+++ src/pages/MyFav.js
@@ -1,18 +1,59 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Jumbotron from 'react-bootstrap/Jumbotron';
+import axios from 'axios';
+import { withAuth0 } from '@auth0/auth0-react';
 
 class MyFav extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      url:process.env.REACT_APP_SERVER,
+      dataList:[],
+      showData:false,
+    }
+
+  }
+
+componentDidMount=async()=>{
+  const {user} = this.props.auth0
+  axios.get(`${this.state.url}/fav?email=${user.email}`).then(result=>{
+    this.setState({
+      dataList:result.data,
+      showData:true,
+    })
+    console.log(result);
+  })
+}
+
+deleteData=async(index)=>{
+  const {user} = this.props.auth0
+
+axios.delete(`${this.state.url}/fav/${index}?email=${user.email}`).then(result=>{
+  this.setState({
+    dataList:result.data,
+    showData:true,
+  })
+  console.log(result);
+})
+}
+
   render() {
     return(
-      <Jumbotron>
-        <h1>My Favorite</h1>
-        <p>
-          This is a collection of my favorites
-        </p>
-      </Jumbotron>
+      <>
+      {this.props.auth0.isAuthenticated &&
+      this.state.showData && this.state.dataList.map((item,index)=>{
+        return(
+          <>
+          <h2>{item.name}</h2>
+          <h2>{item.level}</h2>
+          <img src={item.img} alt='img'/>
+          <button onClick={()=>{this.deleteData(index)}} >Delete</button>
+          </>
+        )
+      })}
+            </>
     )
   }
 }
 
-export default MyFav;
+export default withAuth0(MyFav);
